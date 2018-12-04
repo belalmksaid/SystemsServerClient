8a93926c080a043505c7997f72a684d374169fb8
bennettgberg <bennett.greenberg@rutgers.edu>
2018-12-03 23:34:40 -0500

    should be sending message but can't connect

--- bankingClient.c
+++ bankingClient.c
@@ -4,6 +4,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include "commons.h"
 
 int main(int argc, char ** argv) {
 	if(argc != 3) {
@@ -20,10 +21,40 @@ int main(int argc, char ** argv) {
 		exit(1);	
 	}
         struct hostent * server = gethostbyname(srvname);
+	printf("Host name acquired.\n");
         if (server == NULL) {
             	printf("Error! No host named %s\n", srvname);
             	exit(0);
         }
 //	printf("sockfd: %d\n", sockfd);	
+	struct sockaddr_in serv_addr;
+	memset(&serv_addr, '0', sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(portnum);
+	printf("Attempting to connect...\n");
+	int cnct = connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr));
+    	if (cnct < 0){
+        	error("Error! Could not connect.\n");
+	}
+	printf("Connected to server %s\n", srvname);
+    	printf("Enter command:  ");
+	char buffer[512];
+	memset(&buffer,'0', 512);
+	while(strcmp(buffer, "Shutdown") != 0) {
+    		printf("Enter command:  ");
+		memset(&buffer,'0', 512);
+		fgets(buffer, 511, stdin);
+		int n = write(sockfd,buffer,strlen(buffer));
+		if (n < 0) {
+		     error("Error! Could not write to socket.\n");
+		}
+		bzero(buffer,256);
+		n = read(sockfd, buffer, 255);
+		if (n < 0) {
+		     error("Error! Could not read from socket.\n");
+		}
+		printf("%s\n",buffer);
+	}
+
 	return 0;
 }
